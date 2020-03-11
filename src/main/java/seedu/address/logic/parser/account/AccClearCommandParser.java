8665fb8256a34346626eea6239510c3d8a976a1c
zwasd <f.yue.lim@gmail.com>
2020-03-11 21:50:09 +0800

    renamed account list and clear parser (#57)

--- src/main/java/seedu/address/logic/parser/account/AccClearCommandParser.java
+++ src/main/java/seedu/address/logic/parser/account/AccClearCommandParser.java
@@ -4,10 +4,13 @@ import seedu.address.logic.commands.account.AccClearCommand;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.exceptions.ParseException;
 
-public class ClearCommandParser implements Parser<AccClearCommand> {
-    public ClearCommandParser() {
+
+public class AccClearCommandParser implements Parser<AccClearCommand> {
+
+    public AccClearCommandParser() {
 
     }
+
     @Override
     public AccClearCommand parse(String userInput) throws ParseException {
         return null;
