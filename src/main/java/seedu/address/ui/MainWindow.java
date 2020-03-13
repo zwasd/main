82bcb25555025f2e7cd1514ab4dd651e77df21dd
XinPei-ng <54896139+XinPei-ng@users.noreply.github.com>
2020-03-13 14:47:54 +0800

    Ui (#73)
    * Add UI Image
    
    * Edit README and UserGuide
    
    * Edit DevGuide
    
    * Remove q
    
    * Add an
    
    * Change UI
    
    * Checkstyle
    
    Co-authored-by: Ng Xinpei <ngxinpei@Ngs-MacBook-Pro.local>

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -17,6 +17,7 @@ import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+
 /**
  * The Main Window. Provides the basic application layout containing
  * a menu bar and space where other JavaFX elements can be placed.
@@ -162,6 +163,11 @@ public class MainWindow extends UiPart<Stage> {
 
     public ExpenditureListPanel getExpenditureListPanel() {
         return expenditureListPanel;
+    }
+
+    @FXML
+    private void handleReport() {
+
     }
 
     /**
