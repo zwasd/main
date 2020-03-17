cd61828db1bbad55a6506b611ac6ce08348b2954
XinPei-ng <elainengxp@gmail.com>
2020-03-17 21:52:01 +0800

    Change ExpSetBudget Command and Parser

--- src/main/java/seedu/address/logic/commands/expenditure/ExpSetBudgetCommand.java
+++ src/main/java/seedu/address/logic/commands/expenditure/ExpSetBudgetCommand.java
@@ -4,6 +4,8 @@ import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.expenditure.Amount;
+import seedu.address.model.expenditure.Date;
 
 import java.time.LocalDate;
 
@@ -15,10 +17,10 @@ public class ExpSetBudgetCommand extends Command {
     public static final String COMMAND_WORD = "setbudget";
     public static final String MESSAGE_FAIL = "Budget cannot be set";
 
-    private LocalDate date;
-    private double budget;
+    private Date date;
+    private Amount budget;
 
-    public ExpSetBudgetCommand(LocalDate date, double budget){
+    public ExpSetBudgetCommand(Date date, Amount budget){
 
         this.date = date;
         this.budget = budget;
--- src/main/java/seedu/address/logic/parser/expenditure/ExpSetBudgetCommandParser.java
+++ src/main/java/seedu/address/logic/parser/expenditure/ExpSetBudgetCommandParser.java
@@ -1,40 +1,57 @@
 package seedu.address.logic.parser.expenditure;
 
+import seedu.address.logic.commands.expenditure.ExpAddCommand;
 import seedu.address.logic.commands.expenditure.ExpSetBudgetCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.ParserUtil;
+import seedu.address.logic.parser.Prefix;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.expenditure.Amount;
+import seedu.address.model.expenditure.Date;
+import seedu.address.model.expenditure.Info;
+import seedu.address.model.tag.Tag;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.Set;
+import java.util.stream.Stream;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 /**
  * Parse set budget.
  */
 public class ExpSetBudgetCommandParser implements Parser<ExpSetBudgetCommand> {
-    public ExpSetBudgetCommandParser() {}
+    public ExpSetBudgetCommandParser() {
+    }
+
     @Override
     public ExpSetBudgetCommand parse(String userInput) throws ParseException {
 
-        String userInputTrimmed = userInput.trim();
-        String[] userInputArray = userInputTrimmed.split(" ");
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(userInput, PREFIX_AMOUNT, PREFIX_DATE);
 
-        if(userInputArray.length < 3) {
+        if (!arePrefixesPresent(argMultimap, PREFIX_AMOUNT, PREFIX_AMOUNT)
+                || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ExpSetBudgetCommand.MESSAGE_FAIL));
         }
 
-        double budget;
-        LocalDate date;
+        Amount amount = ParserUtil.parseAmount(argMultimap.getValue(PREFIX_AMOUNT).get());
+        Date date = ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).orElseGet(() -> LocalDate.now().toString()));
 
-        try{
-            budget = Double.parseDouble(userInputArray[1]);
-            date = LocalDate.parse(userInputArray[2], DateTimeFormatter.ofPattern("YYYY-MM"));
-        } catch (Exception e) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ExpSetBudgetCommand.MESSAGE_FAIL));
-        }
+        return new ExpSetBudgetCommand(date, amount);
+    }
 
-        return new ExpSetBudgetCommand(date, budget);
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
