6e6ba90c19abb2bc45373c5a1f97339cee27a98d
ShaoPeng <sp.shaopeng@gmail.com>
2020-03-24 14:52:04 +0800

    Add in test classes for parser and command

---
+++ src/test/java/seedu/address/logic/commands/repeat/RepeatEditCommandTest.java
@@ -0,0 +1,4 @@
+package seedu.address.logic.commands.repeat;
+
+public class RepeatEditCommandTest {
+}
---
+++ src/test/java/seedu/address/logic/parser/repeat/RepeatAddCommandParserTest.java
@@ -0,0 +1,7 @@
+package seedu.address.logic.parser.repeat;
+
+
+public class RepeatAddCommandParserTest {
+    private final RepeatAddCommandParser parser = new RepeatAddCommandParser();
+
+}
