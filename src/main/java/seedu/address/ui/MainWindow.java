e91e1f6fdcd4db3e5d5bec4b4d4588c60b3d5ac9
ShaoPeng <sp.shaopeng@gmail.com>
2020-03-31 16:20:09 +0800

    change according to fengyue suggestion for the center column

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -38,9 +38,8 @@ public class MainWindow extends UiPart<Stage> {
     private ResultDisplay resultDisplay;
     private HelpWindow helpWindow;
     private ReportWindow reportWindow;
-    private ActiveAccountNameView activeAccountNameView;
+    private ActiveNameAndDateView activeNameAndDateView;
     private CalendarView calendarView;
-    private String accountName;
 
     @FXML
     private StackPane commandBoxPlaceholder;
@@ -131,9 +130,9 @@ public class MainWindow extends UiPart<Stage> {
         expenditureListPanelPlaceholder.getChildren().add(repeatListPanel.getRoot());
         calendarView = new CalendarView(this::executeCommand);
         calendar.getChildren().add(calendarView.getRoot());
-        activeAccountNameView = new ActiveAccountNameView(this.accountName);
+        activeNameAndDateView = new ActiveNameAndDateView();
 
-        activeAccountNamePlaceHolder.getChildren().add(activeAccountNameView.getRoot());
+        activeAccountNamePlaceHolder.getChildren().add(activeNameAndDateView.getRoot());
 
 
 
@@ -145,6 +144,7 @@ public class MainWindow extends UiPart<Stage> {
 
         CommandBox commandBox = new CommandBox(this::executeCommand);
         commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
+
     }
 
     /**
@@ -230,16 +230,16 @@ public class MainWindow extends UiPart<Stage> {
             }
 
             if (commandResult.isShowReport()) {
-
                 reportWindow.showResult(commandResult);
             }
 
             if (commandResult.isUpdateCalendar()) {
                 calendarView.updateActiveDate(commandResult.getNewActiveDate());
+                activeNameAndDateView.setActiveDate(commandResult.getNewActiveDate().toString());
             }
 
             if (commandResult.isUpdateAccountName()) {
-                activeAccountNameView.setActiveAccountName(commandResult.getActiveAccountName());
+                activeNameAndDateView.setActiveAccountName(commandResult.getActiveAccountName());
             }
 
             return commandResult;
