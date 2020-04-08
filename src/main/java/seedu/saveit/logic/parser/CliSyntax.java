90f292c4f537c91dd523b9a381546db2625c2538
ShaoPeng <sp.shaopeng@gmail.com>
2020-04-09 02:03:12 +0800

    fiX SOME BUG

--- src/main/java/seedu/saveit/logic/parser/CliSyntax.java
+++ src/main/java/seedu/saveit/logic/parser/CliSyntax.java
@@ -13,8 +13,8 @@ public class CliSyntax {
     public static final Prefix PREFIX_END_DATE = new Prefix("-ed ");
     public static final Prefix PREFIX_PERIOD = new Prefix("-p ");
     public static final Prefix PREFIX_YEARMONTH = new Prefix("-ym ");
-    public static final Prefix PREFIX_GRAPH = new Prefix("-g");
-    public static final Prefix PREFIX_FILENAME = new Prefix("-f");
-    public static final Prefix PREFIX_ORGANISE = new Prefix("-o");
+    public static final Prefix PREFIX_GRAPH = new Prefix("-g ");
+    public static final Prefix PREFIX_FILENAME = new Prefix("-f ");
+    public static final Prefix PREFIX_ORGANISE = new Prefix("-o ");
 
 }
